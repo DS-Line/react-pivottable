51c2f9c25c1273b41950981b56c5b11fd39083fd
Ankit Khatiwada <akhatiwada@robling.io>
2024-12-10 15:52:34 +0545

    fix(Type-Fix): fixed type issue by adding any

---
+++ src/pivot-table.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable  import/no-duplicates */
+
+'use client';
+
+import React, {useState} from 'react';
+
+import './styles.css';
+
+import 'react-pivottable/pivottable.css';
+import PivotTableUI from './../../react-pivottable/src/PivotTableUI';
+import {PivotTableUIProps} from 'react-pivottable/PivotTableUI';
+import createPlotlyRenderers from 'react-pivottable/PlotlyRenderers';
+import TableRenderers from './TableRenderers';
+import Plot from 'react-plotly.js';
+
+export default function LocalPivotTable({
+  data,
+}: {
+  data: {[k: string]: string}[];
+}) {
+  const [requiredProps, setRequiredProps] = useState<
+    PivotTableUIProps & {
+      hideTotals: boolean;
+    }
+  >({
+    data,
+    rows: data.length > 0 ? Object.keys(data[0]) : undefined,
+    cols: undefined,
+    aggregatorName: 'None',
+    vals: undefined,
+    rendererName: undefined,
+    hideTotals: true,
+    onChange: (
+      e: PivotTableUIProps & {
+        hideTotals: boolean;
+      }
+    ) => {
+      setRequiredProps(() => {
+        return {
+          ...e,
+          rows: e.rows?.filter((el) => el !== '227'),
+          cols: e.cols?.filter((el) => el !== '2at'),
+          aggregatorName:
+            e.cols?.filter((el) => el !== '2at').length === 0 ||
+            e.rows?.filter((el) => el !== '227').length === 0
+              ? 'None'
+              : e.aggregatorName === 'None'
+                ? 'Count'
+                : e.aggregatorName,
+        };
+      });
+    },
+    sorters: undefined,
+  });
+
+  const PlotlyRenderers = createPlotlyRenderers(Plot as any); // eslint-disable-line
+  // useEffect(()=>{
+  //   setRequiredProps((e)=>({...e, data:jsonData}))
+  // },[jsonData])
+  return (
+    <div className="pl-8 w-full max-h-[80dvh] h-full overflow-y-auto showSQL">
+      <PivotTableUI
+        //    data={data}
+        renderers={{...TableRenderers, ...PlotlyRenderers}}
+        {...requiredProps}
+      />
+    </div>
+  );
+}
